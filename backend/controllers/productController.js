f55f2e4e090f2a1a321f16b43b2329162cebfac2
Shahzaib <virgo834@gmail.com>
2022-06-16 19:15:49 +0000

    committed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,22 @@
 const Product = require('../models/product');
+const asyncHandler = require('express-async-handler');
 
-// Get All Produtcts.
-const getProducts = async (req, res) => {
+// Get All Products And product with keywords.
+// For the time being we are only filtering by name.
+const getProducts = asyncHandler(async (req, res) => {
   try {
-    const products = await Product.find();
+    const keyword = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+
+    const products = await Product.find({ ...keyword });
+
+    console.log(keyword);
 
     res.status(201).json({
       success: 'pass',
@@ -16,7 +29,7 @@ const getProducts = async (req, res) => {
       message: err.message,
     });
   }
-};
+});
 
 // Add new Product
 // @METHOD POST
@@ -40,29 +53,22 @@ const addProduct = async (req, res) => {
 };
 
 //Get Product by ID
-const getProductByID = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
+const getProductByID = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-    if (!product) {
-      res.status(404).json({
-        success: 'fail',
-        message: 'Product not found. Check your id',
-      });
-
-      res.status(200).json({
-        success: 'pass',
-        message: 'Product found',
-        product,
-      });
-    }
-  } catch (err) {
-    res.status(400).json({
+  if (!product) {
+    res.status(404).json({
       success: 'fail',
-      message: err.message,
+      message: 'Product not found. Check your id',
     });
   }
-};
+
+  res.status(200).json({
+    success: 'pass',
+    message: 'Product found',
+    product,
+  });
+});
 
 //Update Product By ID
 //@METHOD POST
@@ -95,15 +101,14 @@ const deleteProduct = async (req, res) => {
         success: 'fail',
         message: 'Product not found. Check your id',
       });
-
-      await product.remove();
-
-      res.status(200).json({
-        success: 'true',
-        message: 'Product is deleted',
-        product,
-      });
     }
+    await product.remove();
+
+    res.status(200).json({
+      success: 'true',
+      message: 'Product is deleted',
+      product,
+    });
   } catch (err) {
     res.status(400).json({
       success: 'false',
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const products = require('./routes/product');
+const users = require('./routes/user');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -14,6 +15,7 @@ connectDB();
 // Middlewares.
 app.use(express.json());
 app.use('/api/products', products);
+app.use('/api/users', users);
 
 PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
